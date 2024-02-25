3ce4b2ee35b0eaf07ec0719a06de34e5004e5f78
k2 <izmail.k2@gmail.com>
2024-02-25 16:51:29 +0300

    feat(Main): Add image links to mainReducer state

--- src/features/2-main/model/mainReducer.ts
+++ src/features/2-main/model/mainReducer.ts
@@ -4,57 +4,69 @@ import chairInt from '@/assets/img/item/chairs/chair-grey-inter.jpg'
 
 
 type ItemMainType = {
-  id:number,
+  id: number,
   title: string,
   img: string[],
   description: string,
-  category:string,
-  price:string,
+  category: string,
+  price: string,
 }
-type InitialStateType={
+type InitialStateType = {
   items: ItemMainType[]
 }
 const initialState: InitialStateType = {
   items: [
     {
-      id:1,
+      id: 1,
       title: 'Chair grey',
-      img: [ chairInt, chair,],
+      img: [chairInt, chair,],
       description: 'Nice chair, to by, not pass',
-      category:'chairs',
-      price:'49.99',
+      category: 'chairs',
+      price: '49.99',
     },
     {
-      id:2,
+      id: 2,
       title: 'Table',
-      img: ['table.webp'],
+      img: [
+        'https://i1.adis.ws/i/fv/PRODSAIGXTB-MT-001_saigon_extending-dining-table-metal-base__lifestyle?$medium$&w=579&fmt=webp',
+        'https://i1.adis.ws/i/fv/PRODSAIGXTB-MT-001_saigon_extending-dining-table-metal-base__alt04?$product$&fmt=webp&h=1338&w=1898'
+      ],
       description: 'Table a big, nice and cool, the good table if not by else go out',
-      category:'tables',
-      price:'149.99',
+      category: 'tables',
+      price: '149.99',
     },
     {
-      id:3,
+      id: 3,
       title: 'Sofa',
-      img: ['sofa.jpg'],
+      img: [
+        'https://i1.adis.ws/i/fv/PRODZFRSP000000000027556_legend_classic-back-fabric-corner-sofa__lifestyle?$large$&w=689&fmt=webp',
+        'https://i1.adis.ws/i/fv/PRODZFRSP000000000027556_legend_classic-back-fabric-corner-sofa_kingston-silver_right-hand-facing?$product$&fmt=webp&h=2790&w=3964'
+      ],
       description: 'Staying at home on sofa and you not sick never',
-      category:'sofa',
-      price:'549.99',
+      category: 'sofa',
+      price: '549.99',
     },
     {
-      id:4,
+      id: 4,
       title: 'Lamp',
-      img: ['wall-light.jpg'],
+      img: [
+        'https://i1.adis.ws/i/fv/PRODZFRSP000000000046363_elon-stacked-table-lamp_light__lifestyle?$large$&w=689&fmt=webp&fmt=webp',
+      'https://i1.adis.ws/i/fv/PRODZFRSP000000000046363_elon-stacked-table-lamp_light_champagne-gold?$product$&w=1981.5&fmt=webp'
+      ],
       description: 'On lamp and see us, off lamp the bad',
-      category:'sofa',
-      price:'549.99',
+      category: 'lighting',
+      price: '549.99',
     },
     {
-      id:5,
+      id: 5,
       title: 'Chair white',
-      img: ['chair-white.jpeg'],
+      img: [
+        'https://i1.adis.ws/i/fv/PRODAVLNCHPR--001_avellino_dining-chairs-pair__lifestyle?$large$&w=689&fmt=webp&fmt=webp',
+      'https://i1.adis.ws/i/fv/PRODAVLNCHPR--001_avellino_dining-chairs-pair?$product$&w=1981.5&fmt=webp'
+      ],
       description: 'Chair white so nice also sun',
-      category:'sofa',
-      price:'549.99',
+      category: 'sofa',
+      price: '549.99',
     },
   ]
 }
